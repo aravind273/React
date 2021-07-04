f790b730ccd53e9ec6c49da5c4479ef736a5cfe9
Aravind Eramalla <42810684+aravind273@users.noreply.github.com>
2021-07-04 15:28:04 +0530

    Create App.js

---
+++ React components/src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Heading from "./Heading.jsx";
+import List from "./List.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
