04de5cf82137b8e712dbe0baa5870b1a163fc82a
Aravind Eramalla <42810684+aravind273@users.noreply.github.com>
2021-07-04 15:30:12 +0530

    Create index.js

---
+++ React components/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "./styles.css";
+import "./Heading.jsx";
+import App from "./App.js";
+
+ReactDom.render(<App />, document.getElementById("root"));
